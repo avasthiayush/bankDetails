f725d9b454dfc211b1ec1eefb7a0ea55d796e6f3
avasthiayush <avasthiayush22@gmail.com>
2021-12-21 19:03:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Banks from './components/Banks'
+
+function App() {
+  return (
+    <div>
+     <Banks/>
+    </div>
+  );
+}
+
+export default App;
